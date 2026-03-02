0a181adc9d43b8de49593ed9ac6ab4711c39b07f
xobazjr <MR.TeerapatKotanartChannel@Gmail.COM>
2026-03-02 11:49:28 +0700

    Update landing_page.dart
    ✅ Change function name NavigationExample to Navigation

--- mobile_app/lib/landing_page.dart
+++ mobile_app/lib/landing_page.dart
@@ -79,18 +79,18 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: NavigationExample());
+    return const MaterialApp(home: Navigation());
   }
 }
 
-class NavigationExample extends StatefulWidget {
-  const NavigationExample({super.key});
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
 
   @override
-  State<NavigationExample> createState() => _NavigationExampleState();
+  State<Navigation> createState() => _NavigationState();
 }
 
-class _NavigationExampleState extends State<NavigationExample> {
+class _NavigationState extends State<Navigation> {
   int currentPageIndex = 0;
   int _selectedIndex = 0;
 

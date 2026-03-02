d43acd889ac34f6dacb1c311fdc2f83987b0f9d6
Phongsiri Loedphongthai <p.loedphongthai@email.kmutnb.ac.th>
2026-03-02 17:20:49 +0700

    [backend] get details from patient

--- backend/app/api/patients/list/route.js
+++ backend/app/api/patients/list/route.js
@@ -5,21 +5,11 @@ import bcrypt from 'bcryptjs';
 export async function GET(req) {
     try {
 
-        let patients = [];
         const { searchParams } = new URL(req.url);
         const caretaker = searchParams.get('caretaker_name');
         const find_caretaker = await sql`SELECT u.user_id FROM users u WHERE u.username = ${caretaker}`;
-        const find_patient_from_caretaker = await sql`SELECT u.user_id FROM users u WHERE u.caretaker_id = ${find_caretaker[0].user_id}`;
-
-        for (let i = 0; i < find_patient_from_caretaker.length; i++) {
-            const userId = find_patient_from_caretaker[i].user_id;
-            const user = await sql`select * from users u where u.user_id = ${userId}`;
-            patients.push({
-               username: user[0].username
-            });
-        }
-
-        return NextResponse.json(patients);
+        const find_patient_from_caretaker = await sql`SELECT u.username FROM users u WHERE u.caretaker_id = ${find_caretaker[0].user_id}`;
+        return NextResponse.json(find_patient_from_caretaker);
 
     } catch (e) {
         return NextResponse.json(

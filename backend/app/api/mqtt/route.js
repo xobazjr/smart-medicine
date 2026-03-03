2032b21e6a54778ef6a3bfd325f8bb080b87a9ed
Phongsiri Loedphongthai <p.loedphongthai@email.kmutnb.ac.th>
2026-03-03 20:50:54 +0700

    [be] fix api mqtt status update

--- backend/app/api/mqtt/route.js
+++ backend/app/api/mqtt/route.js
@@ -5,15 +5,16 @@ export async function POST(req) {
     try {
         const body = await req.json();
 
-        const { msg, time } = body;
-        //
-        // if (msg !== "I'm alive") {
-        //     return NextResponse.json(
-        //         { status: 400, error: "Bad request" },
-        //         { status: 400 }
-        //     );
-        // }
-        await sql`UPDATE board_status SET timestamp = ${time} WHERE name = 'esp8266_xbjr'`;
+        const { timestamp } = body;
+
+        if (!timestamp) {
+            return NextResponse.json({
+                status: 400,
+                error: "Key 'timestamp' not found in request body"
+            }, { status: 400 });
+        }
+
+        await sql`UPDATE board_status SET timestamp = ${timestamp} WHERE name = 'esp8266_xbjr'`;
         return NextResponse.json({ status: 200, success: true }, { status: 200 } )
     } catch (e) {
         return NextResponse.json({
